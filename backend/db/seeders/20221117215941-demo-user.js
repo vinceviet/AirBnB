7923b197f84e7cb472d873ebd7ca173788284ee1
vinceviet <vince.viet@gmail.com>
2022-11-18 09:43:01 -0800

    validate fix

--- backend/db/seeders/20221117215941-demo-user.js
+++ backend/db/seeders/20221117215941-demo-user.js
@@ -11,16 +11,22 @@ module.exports = {
     options.tableName = 'Users';
     return queryInterface.bulkInsert(options, [
       {
+        firstName: 'demo',
+        lastName: 'demo',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'fake',
+        lastName: 'fake',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'fake2',
+        lastName: 'fake2',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
