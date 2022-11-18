1f2a766cd21905ec7de2801b113b53010285258d
vinceviet <vince.viet@gmail.com>
2022-11-18 13:50:49 -0800

    seeder update

---
+++ backend/db/seeders/20221117215941-starter-user.js
@@ -0,0 +1,44 @@
+'use strict';
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    return queryInterface.bulkInsert(options, [
+      {
+        firstName: 'Luffy',
+        lastName: 'Monkey',
+        email: 'kaizokunoou@grandline.com',
+        username: 'Mugiwara',
+        hashedPassword: bcrypt.hashSync('nakama4life')
+      },
+      {
+        firstName: 'Zoro',
+        lastName: 'Roronoa',
+        email: 'santoryu@grandline.com',
+        username: 'Santoryu',
+        hashedPassword: bcrypt.hashSync('iamlost')
+      },
+      {
+        firstName: 'Robin',
+        lastName: 'Nico',
+        email: 'historybuff@grandline.com',
+        username: 'Hanahana',
+        hashedPassword: bcrypt.hashSync('oharalives')
+      }
+    ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Mugiwara', 'Santoryu', 'Hanahana'] }
+    }, {});
+  }
+};
