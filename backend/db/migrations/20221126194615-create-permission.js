bd1833fe4c0dcb4b1ea72f48fc323412f9adadfc
vinceviet <vince.viet@gmail.com>
2022-11-26 11:54:20 -0800

    added permissions model

---
+++ backend/db/migrations/20221126194615-create-permission.js
@@ -0,0 +1,39 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+};
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Permissions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'Users' }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Permissions'
+    await queryInterface.dropTable(options);
+  }
+};
