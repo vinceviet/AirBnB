975701f7d0fb995d5346960ccb789ffa1a64a91d
vinceviet <vince.viet@gmail.com>
2022-12-09 08:51:03 -0800

    frontend auth setup

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,4 +1,5 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { combineReducers, applyMiddleware, compose } from "redux";
+import { legacy_createStore as createStore } from "redux";
 import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
