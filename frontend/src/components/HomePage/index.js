02f0dd43c6317ed8629896ba3d4facc8ce79d707
vinceviet <vince.viet@gmail.com>
2022-12-11 19:51:34 -0800

    component folders added

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -1,5 +1,7 @@
+// import { NavLink } from 'react-router-dom';
+import './HomePage.css'
 
-export default function HomePage () {
+export default function HomePage() {
     return (
         <h1>Home Page</h1>
     );
