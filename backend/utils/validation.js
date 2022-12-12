0b3c0b9f075f0f431cf1306c54e7631cec5a0abf
vinceviet <vince.viet@gmail.com>
2022-12-12 14:41:53 -0800

    validation fix

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -6,18 +6,25 @@ const handleValidationErrors = (req, _res, next) => {
     const validationErrors = validationResult(req);
 
     if (!validationErrors.isEmpty()) {
-        //     .array()
-        //     .map((error) => `${error.msg}`);
-        const errorList = {};
-        const errors = validationErrors;
-        const err = Error("Validation error");
-        err.status = 400;
-        err.errors = errorList
-        for (let error of errors.array()) {
-            errorList[error.param] = error.msg
-        }
-        next(err);
+        const errors = validationErrors
+        .array()
+        .map((error) => `${error.msg}`);
+
+      const err = Error('Bad request.');
+      err.errors = errors;
+      err.status = 400;
+      next(err);
     }
+        // const errorList = {};
+        // const errors = validationErrors;
+        // const err = Error("Validation error");
+        // err.status = 400;
+        // err.errors = errorList
+        // for (let error of errors.array()) {
+        //     errorList[error.param] = error.msg
+        // }
+        // next(err);
+    // }
     next();
 };
 
