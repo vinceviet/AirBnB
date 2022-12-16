a9462cd330a37fe064b387933e08fb310679ada3
vinceviet <vince.viet@gmail.com>
2022-12-16 14:48:09 -0800

    seed fix

--- backend/db/seeders/20221121203034-starter-reviews.js
+++ backend/db/seeders/20221121203034-starter-reviews.js
@@ -13,109 +13,109 @@ module.exports = {
       {
         spotId: 1,
         userId: 3,
-        review: 'Nice and cozy. There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Nice and cozy. There was a leak in the boat.',
         stars: 5
       },
       {
         spotId: 2,
         userId: 3,
-        review: "The house was bit dirty, lake was really low. There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ",
+        review: "The house was bit dirty, lake was really low. There was a leak in the boat.",
         stars: 2
       },
       {
         spotId: 3,
         userId: 3,
-        review: 'Great spot, great people and ducks. There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Great spot, great people and ducks. There was a leak in the boat.',
         stars: 5
       },
       {
         spotId: 1,
         userId: 2,
-        review: 'Great place to get away. There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Great place to get away. There was a leak in the boat.',
         stars: 4
       },
       {
         spotId: 2,
         userId: 2,
-        review: "Not the best, but not the worst place. There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ",
+        review: "Not the best, but not the worst place. There was a leak in the boat.",
         stars: 3
       },
       {
         spotId: 3,
         userId: 2,
-        review: 'Too big, got lost.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Too big, got lost.There was a leak in the boat.',
         stars: 3
       },
       {
         spotId: 4,
         userId: 1,
-        review: 'Love staying in high places, treehouse was great.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Love staying in high places, treehouse was great.There was a leak in the boat.',
         stars: 5
       },
       {
         spotId: 4,
         userId: 3,
-        review: 'Was hard to find the spot.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Was hard to find the spot.There was a leak in the boat.',
         stars: 2
       },
       {
         spotId: 5,
         userId: 3,
-        review: 'Place too big, got lost.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Place too big, got lost.There was a leak in the boat.',
         stars: 2
       },
       {
         spotId: 5,
         userId: 1,
-        review: 'Great lake view.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Great lake view.There was a leak in the boat.',
         stars: 5
       },
       {
         spotId: 6,
         userId: 1,
-        review: 'Plenty of space, nice and clean.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Plenty of space, nice and clean.There was a leak in the boat.',
         stars: 4
       },
       {
         spotId: 6,
         userId: 3,
-        review: 'Great food and drink options.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Great food and drink options.There was a leak in the boat.',
         stars: 5
       },
       {
         spotId: 7,
         userId: 1,
-        review: 'Great open space, great county.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Great open space, great county.There was a leak in the boat.',
         stars: 5
       },
       {
         spotId: 7,
         userId: 2,
-        review: 'Not enough amenities.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Not enough amenities.There was a leak in the boat.',
         stars: 3
       },
       {
         spotId: 8,
         userId: 2,
-        review: 'Very quiet, perfect place to read.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Very quiet, perfect place to read.There was a leak in the boat.',
         stars: 5
       },
       {
         spotId: 8,
         userId: 1,
-        review: 'Too quiet, not enough action.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ',
+        review: 'Too quiet, not enough action.There was a leak in the boat.',
         stars: 2
       },
       {
         spotId: 9,
         userId: 1,
-        review: "Forgot I can't swim, almost drowned.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ",
+        review: "Forgot I can't swim, almost drowned.There was a leak in the boat.",
         stars: 1
       },
       {
         spotId: 9,
         userId: 2,
-        review: "Place was nice but too many drowning people, had to play lifegaurd.There was a leak in the boat. Nobody had yet noticed it, and nobody would for the next couple of hours. This was a problem since the boat was heading out to sea and while the leak was quite small at the moment, it would be much larger when it was ultimately discovered. ",
+        review: "Place was nice but too many drowning people, had to play lifegaurd.There was a leak in the boat.",
         stars: 4
       }
     ], {})
