bfa472681bee122a62689993c58ca2ee686f3ee8
vinceviet <vince.viet@gmail.com>
2022-12-17 19:24:20 -0800

    css

--- frontend/src/components/DeleteReviewModal/index.js
+++ frontend/src/components/DeleteReviewModal/index.js
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
 import { deleteReview } from '../../store/reviewsReducer';
 import { getSpotDetails } from '../../store/spotsReducer';
